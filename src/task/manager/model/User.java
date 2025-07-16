6de16a49cdafd5b0c6e0cf612fef1911c57cb7d6
Arsh <qarsh273@gmail.com>
2025-07-16 22:57:15 +0530

    update the user.java file added the getters and setters, configured the password getter and setter to use the encrypted password utility.

--- src/task/manager/model/User.java
+++ src/task/manager/model/User.java
@@ -2,11 +2,37 @@ package task.manager.model;
 
 import java.util.Date;
 
+import task.manager.utility.PasswordUtility;
+
 public class User {
 
 	private String username;
 	private String password;
 	private Date timestampAccountCreated;
 	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setPassword(String planPassword) {
+		this.password = PasswordUtility.encryptPassword(planPassword);
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	
+	public void setTimestampAccountCreated(Date timestampAccountCreated) {
+		this.timestampAccountCreated = timestampAccountCreated;
+	}
+	
+	public Date getTimestampAccountCreated() {
+		return timestampAccountCreated;
+	}
 	
 }
